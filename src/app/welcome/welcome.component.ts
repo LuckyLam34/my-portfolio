5d4e13a8685bd1c709cd07a0c58861b88e225025
Lucky <tutrinh3004@gmail.com>
2017-10-15 23:19:34 +0700

    welcome page

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'trinhlam-portfolio-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
